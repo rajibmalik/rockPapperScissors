55877cb3eb1763b92ca155534dde0906d055daa9
rajibmalik <rajibmalik@hotmail.co.uk>
2022-12-22 19:43:10 +0000

    Function that randomly returns rock, paper or scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,13 @@
 console.log("hello world")
+
+// game against computer
+// start with function getComputerChoice
+// randomly returns either "Rock", "Paper", or "Scissors"
+
+const options = ["Rock", "Paper", "Scissors"];
+
+const getComputerChoice = () => {
+  return options[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice());
