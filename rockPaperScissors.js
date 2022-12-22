97b26815a19026a85766ebb5cd5512c67c5e6992
rajibmalik <rajibmalik@hotmail.co.uk>
2022-12-22 20:31:51 +0000

    Function that plays a round of rock, paper, scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,13 +1,50 @@
-console.log("hello world")
+// console.log("hello world")
 
 // game against computer
 // start with function getComputerChoice
 // randomly returns either "Rock", "Paper", or "Scissors"
 
-const options = ["Rock", "Paper", "Scissors"];
+const options = ["rock", "paper", "scissors"];
 
 const getComputerChoice = () => {
   return options[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+// function that plays a round of rock paper scissors
+// two parameters
+// playerSelection 
+// computerSelection 
+
+// return a string that declares the winner of the round 
+// e.g "You Lose! Paper beats Rock"
+
+// Make playerSelection case-insensitive, user can input rock, ROCK, RocK 
+
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase();
+  if (
+    playerSelection == "rock" && computerSelection == "scissors" ||
+    playerSelection == "scissors" && computerSelection == "paper" ||
+    playerSelection == "paper" && computerSelection == "rock"
+    ){
+    return `You win! ${playerSelection} beats ${computerSelection}`
+  } else if (
+    computerSelection == "rock" && playerSelection == "scissors" ||
+    computerSelection == "scissors" && playerSelection == "paper" || 
+    computerSelection == "paper" && playerSelection == "rock"
+  ){ 
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `Draw! ${playerSelection} and ${computerSelection} means that there is no winner!`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = "rock"
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
